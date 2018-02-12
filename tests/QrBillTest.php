f005e8e45692900c1081ece0c9a5ba287831a8df
Manuel Reinhard <manu@sprain.ch>
2018-02-12 22:11:31 +0100

    Fix tests

--- tests/QrBillTest.php
+++ tests/QrBillTest.php
@@ -15,61 +15,483 @@ use Sprain\SwissQrBill\QrBill;
 
 class QrBillTest extends TestCase
 {
-    /** @var  QrBill */
-    protected $qrBill;
-
     public function testMinimalSetupIsValid()
     {
-        $qrBill = (new QrBill())
-            ->setHeader($this->createMock(Header::class))
-            ->setCreditorInformation($this->createMock(CreditorInformation::class))
-            ->setCreditor($this->createMock(Creditor::class))
-            ->setPaymentAmountInformation($this->createMock(PaymentAmountInformation::class))
-            ->setPaymentReference($this->createMock(PaymentReference::class));
+        $qrBill = QrBill::create();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
 
         $this->assertTrue($qrBill->isValid());
     }
 
-    public function testHeaderIsSetInDefault()
+    public function testCreditorInformationIsRequired()
     {
-        $qrBill = (QrBill::create())
-            ->setCreditorInformation($this->createMock(CreditorInformation::class))
-            ->setCreditor($this->createMock(Creditor::class))
-            ->setPaymentAmountInformation($this->createMock(PaymentAmountInformation::class))
-            ->setPaymentReference($this->createMock(PaymentReference::class));
+        $qrBill = QrBill::create();
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $this->assertSame(1, $qrBill->getViolations()->count());
+    }
+
+    public function testCreditorInformationMustBeValid()
+    {
+        $qrBill = QrBill::create();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('INVALIDIBAN');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $this->assertFalse($qrBill->isValid());
+    }
+
+    public function testCreditorIsRequired()
+    {
+        $qrBill = QrBill::create();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $this->assertSame(1, $qrBill->getViolations()->count());
+    }
+
+    public function testCreditorMustBeValid()
+    {
+        $qrBill = QrBill::create();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            // NO NAME!
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $this->assertFalse($qrBill->isValid());
+    }
+
+    public function testPaymentAmountInformationIsRequired()
+    {
+        $qrBill = QrBill::create();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $this->assertSame(1, $qrBill->getViolations()->count());
+    }
+
+    public function testPaymentAmountInformationMustBeValid()
+    {
+        $qrBill = QrBill::create();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('USD') // INVALID CURRENCY
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $this->assertFalse($qrBill->isValid());
+    }
+
+    public function testPaymentReferenceIsRequired()
+    {
+        $qrBill = QrBill::create();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $this->assertSame(1, $qrBill->getViolations()->count());
+    }
+
+    public function testPaymentReferenceMustBeValid()
+    {
+        $qrBill = QrBill::create();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('INVALID REFERENCE');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $this->assertFalse($qrBill->isValid());
+    }
+
+    public function testHeaderIsRequired()
+    {
+        $qrBill = new QrBill();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $this->assertSame(1, $qrBill->getViolations()->count());
+    }
+
+    public function testHeaderMustBeValid()
+    {
+        $qrBill = new QrBill();
+
+        $header = new Header();
+        $qrBill->setHeader($header); // INVALID EMPTY HEADER
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $this->assertFalse($qrBill->isValid());
+    }
+
+    public function testOptionalUltimateCreditorCanBeSet()
+    {
+        $qrBill = QrBill::create();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $ultimateCreditor = (new UltimateCreditor())
+            ->setName('My Company Holding Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setUltimateCreditor($ultimateCreditor);
 
         $this->assertTrue($qrBill->isValid());
     }
 
-    public function testAllRequiredPropertiesMustBeSet()
+    public function testOptionalUltimateCreditorMustBeValid()
     {
-        $requiredProperties = 5;
+        $qrBill = QrBill::create();
 
-        for ($i = 1; $i <= $requiredProperties; $i++) {
-            $qrBill = new QrBill();
-            ($i == 1) ?: $qrBill->setHeader($this->createMock(Header::class));
-            ($i == 2) ?: $qrBill->setCreditorInformation($this->createMock(CreditorInformation::class));
-            ($i == 3) ?: $qrBill->setCreditor($this->createMock(Creditor::class));
-            ($i == 4) ?: $qrBill->setPaymentAmountInformation($this->createMock(PaymentAmountInformation::class));
-            ($i == 5) ?: $qrBill->setPaymentReference($this->createMock(PaymentReference::class));
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
 
-            $this->assertSame(1, $qrBill->getViolations()->count(), sprintf('Failed with property number %s.', $i));
-        }
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $ultimateCreditor = (new UltimateCreditor())
+            // NO NAME!
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setUltimateCreditor($ultimateCreditor);
+
+        $this->assertFalse($qrBill->isValid());
     }
 
-    public function testAllOptionPropertiesAreAccepted()
+    public function testOptionalUltimateDebtorCanBeSet()
     {
-        $qrBill = (new QrBill())
-            ->setHeader($this->createMock(Header::class))
-            ->setCreditorInformation($this->createMock(CreditorInformation::class))
-            ->setCreditor($this->createMock(Creditor::class))
-            ->setPaymentAmountInformation($this->createMock(PaymentAmountInformation::class))
-            ->setPaymentReference($this->createMock(PaymentReference::class))
+        $qrBill = QrBill::create();
 
-            ->setUltimateCreditor($this->createMock(UltimateCreditor::class))
-            ->setUltimateDebtor($this->createMock(UltimateDebtor::class))
-            ->setAlternativeSchemes([$this->createMock(AlternativeScheme::class)]);
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $ultimateDebtor = (new UltimateDebtor())
+            ->setName('Thomas LeClaire')
+            ->setStreet('Rue examplaire')
+            ->setHouseNumber('22a')
+            ->setPostalCode('1000')
+            ->setCity('Lausanne')
+            ->setCountry('CH');
+        $qrBill->setUltimateDebtor($ultimateDebtor);
 
         $this->assertTrue($qrBill->isValid());
+    }
+
+    public function testOptionalUltimateDebtorMustBeValid()
+    {
+        $qrBill = QrBill::create();
+
+        $creditorInformation = (new CreditorInformation())
+            ->setIban('CH9300762011623852957');
+        $qrBill->setCreditorInformation($creditorInformation);
+
+        $creditor = (new Creditor())
+            ->setName('My Company Ltd.')
+            ->setStreet('Bahnhofstrasse')
+            ->setHouseNumber('1')
+            ->setPostalCode('8000')
+            ->setCity('Zürich')
+            ->setCountry('CH');
+        $qrBill->setCreditor($creditor);
+
+        $paymentAmountInformation = (new PaymentAmountInformation())
+            ->setAmount(25.90)
+            ->setCurrency('CHF')
+            ->setDueDate(new \DateTime('+30 days'));
+        $qrBill->setPaymentAmountInformation($paymentAmountInformation);
+
+        $paymentReference = (new PaymentReference())
+            ->setType(PaymentReference::TYPE_QR)
+            ->setReference('123456789012345678901234567');
+        $qrBill->setPaymentReference($paymentReference);
+
+        $ultimateDebtor = (new UltimateDebtor())
+            // NO NAME!
+            ->setStreet('Rue examplaire')
+            ->setHouseNumber('22a')
+            ->setPostalCode('1000')
+            ->setCity('Lausanne')
+            ->setCountry('CH');
+        $qrBill->setUltimateDebtor($ultimateDebtor);
+
+        $this->assertFalse($qrBill->isValid());
     }
 }
