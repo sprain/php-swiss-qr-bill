c1d5f984f778cc465ab8a0071ab720cc435eec45
Manuel Reinhard <manu@sprain.ch>
2018-12-14 20:21:02 +0100

    Add combined address data group

---
+++ src/DataGroups/CombinedAddress.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Sprain\SwissQrBill\DataGroups;
+
+use Sprain\SwissQrBill\DataGroups\Abstracts\Address;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+
+class CombinedAddress extends Address
+{
+    const ADDRESS_TYPE = 'K';
+
+    /**
+     * Address line 1
+     *
+     * Street and building number or P.O. Box
+     *
+     * @var string
+     */
+    private $addressLine1;
+
+    /**
+     * Address line 2
+     *
+     * Postal code and town
+     *
+     * @var string
+     */
+    private $addressLine2;
+
+    public function getAddressLine1(): ?string
+    {
+        return $this->addressLine1;
+    }
+
+    public function setAddressLine1(string $addressLine1 = null): self
+    {
+        $this->addressLine1 = $addressLine1;
+
+        return $this;
+    }
+
+    public function getAddressLine2(): ?string
+    {
+        return $this->addressLine2;
+    }
+
+    public function setAddressLine2(string $addressLine2): self
+    {
+        $this->addressLine2 = $addressLine2;
+
+        return $this;
+    }
+
+    public function getFullAddress() : string
+    {
+        $address = $this->getName();
+
+        if ($this->getAddressLine1()) {
+            $address .= "\n" . $this->getAddressLine1();
+        }
+
+        $address .= sprintf("\n%s-%s", $this->getCountry(), $this->getAddressLine2());
+
+        return $address;
+    }
+
+    public function getQrCodeData() : array
+    {
+        return [
+            self::ADDRESS_TYPE,
+            $this->getName(),
+            $this->getAddressLine1(),
+            $this->getAddressLine2(),
+            '',
+            '',
+            $this->getCountry()
+        ];
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraints('addressLine1', [
+            new Assert\Length([
+                'max' => 70
+            ])
+        ]);
+
+        $metadata->addPropertyConstraints('addressLine2', [
+            new Assert\NotBlank(),
+            new Assert\Length([
+                'max' => 70
+            ])
+        ]);
+    }
+}
---
+++ tests/DataGroups/CombinedAddressTest.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace Sprain\SwissQrBill\Tests\DataGroups;
+
+use PHPUnit\Framework\TestCase;
+use Sprain\SwissQrBill\DataGroups\CombinedAddress;
+
+class CombinedAddressTest extends TestCase
+{
+    /** @var CombinedAddress */
+    private $address;
+
+    public function setUp()
+    {
+        // Valid default to be adjusted in single tests
+        $this->address = (new CombinedAddress())
+            ->setName('Thomas Mustermann')
+            ->setAddressLine1('Musterweg 22a')
+            ->setAddressLine2('1000 Lausanne')
+            ->setCountry('CH');
+    }
+
+    /**
+     * @dataProvider validNameProvider
+     */
+    public function testNameIsValid($value)
+    {
+        $this->address->setName($value);
+
+        $this->assertSame(0, $this->address->getViolations()->count());
+    }
+
+    public function validNameProvider()
+    {
+        return [
+            ['A'],
+            ['123'],
+            ['Müller AG'],
+            ['Maria Bernasconi'],
+            ['70 chars, character limit abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqr']
+        ];
+    }
+
+    /**
+     * @dataProvider invalidNameProvider
+     */
+    public function testNameIsInvalid($value)
+    {
+        $this->address->setName($value);
+
+        $this->assertSame(1, $this->address->getViolations()->count());
+    }
+
+    public function invalidNameProvider()
+    {
+        return [
+            [''],
+            ['71 chars, above character limit abcdefghijklmnopqrstuvwxyzabcdefghijklm'],
+        ];
+    }
+
+    /**
+     * @dataProvider validAddressLine1Provider
+     */
+    public function testAddressLine1IsValid($value)
+    {
+        $this->address->setAddressLine1($value);
+
+        $this->assertSame(0, $this->address->getViolations()->count());
+    }
+
+    public function validAddressLine1Provider()
+    {
+        return [
+            [null],
+            [''],
+            ['A'],
+            ['123'],
+            ['Sonnenweg'],
+            ['70 chars, character limit abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqr']
+        ];
+    }
+
+    /**
+     * @dataProvider invalidAddressLine1Provider
+     */
+    public function testAddressLine1IsInvalid($value)
+    {
+        $this->address->setAddressLine1($value);
+
+        $this->assertSame(1, $this->address->getViolations()->count());
+    }
+
+    public function invalidAddressLine1Provider()
+    {
+        return [
+            ['71 chars, above character limit abcdefghijklmnopqrstuvwxyzabcdefghijklm'],
+        ];
+    }
+
+    /**
+     * @dataProvider validAddressLine2Provider
+     */
+    public function testAddressLine2IsValid($value)
+    {
+        $this->address->setAddressLine2($value);
+
+        $this->assertSame(0, $this->address->getViolations()->count());
+    }
+
+    public function validAddressLine2Provider()
+    {
+        return [
+            ['A'],
+            ['123'],
+            ['Sonnenweg'],
+            ['70 chars, character limit abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqr']
+        ];
+    }
+
+    /**
+     * @dataProvider invalidAddressLine2Provider
+     */
+    public function testAddressLine2IsInvalid($value)
+    {
+        $this->address->setAddressLine2($value);
+
+        $this->assertSame(1, $this->address->getViolations()->count());
+    }
+
+    public function invalidAddressLine2Provider()
+    {
+        return [
+            ['71 chars, above character limit abcdefghijklmnopqrstuvwxyzabcdefghijklm'],
+        ];
+    }
+
+    public function testAddressLine2IsRequired()
+    {
+        $this->address->setAddressLine2('');
+
+        $this->assertSame(1, $this->address->getViolations()->count());
+    }
+
+    /**
+     * @dataProvider validCountryProvider
+     */
+    public function testCountryIsValid($value)
+    {
+        $this->address->setCountry($value);
+
+        $this->assertSame(0, $this->address->getViolations()->count());
+    }
+
+    public function validCountryProvider()
+    {
+        return [
+            ['CH'],
+            ['ch'],
+            ['DE'],
+            ['LI'],
+            ['US']
+        ];
+    }
+
+    /**
+     * @dataProvider invalidCountryProvider
+     */
+    public function testCountryIsInvalid($value)
+    {
+        $this->address->setCountry($value);
+
+        $this->assertSame(1, $this->address->getViolations()->count());
+    }
+
+    public function invalidCountryProvider()
+    {
+        return [
+            [''],
+            ['XX'],
+            ['SUI'],
+            ['12']
+        ];
+    }
+
+    public function testQrCodeData()
+    {
+        $expected = [
+            'K',
+            'Thomas Mustermann',
+            'Musterweg 22a',
+            '1000 Lausanne',
+            '',
+            '',
+            'CH',
+        ];
+
+        $this->assertSame($expected, $this->address->getQrCodeData());
+    }
+
+    /**
+     * @dataProvider addressProvider
+     */
+    public function testFullAddressString(CombinedAddress $address, $expected)
+    {
+        $this->assertSame($expected, $address->getFullAddress());
+    }
+
+    public function addressProvider()
+    {
+        return [
+            [
+                $this->address = (new CombinedAddress())
+                    ->setName('Thomas Mustermann')
+                    ->setAddressLine1('Musterweg 22a')
+                    ->setAddressLine2('1000 Lausanne')
+                    ->setCountry('CH'),
+                "Thomas Mustermann\nMusterweg 22a\nCH-1000 Lausanne"
+            ],
+            [
+                $this->address = (new CombinedAddress())
+                    ->setName('Thomas Mustermann')
+                    ->setAddressLine1('Musterweg')
+                    ->setAddressLine2('1000 Lausanne')
+                    ->setCountry('CH'),
+                "Thomas Mustermann\nMusterweg\nCH-1000 Lausanne"
+            ],
+            [
+                $this->address = (new CombinedAddress())
+                    ->setName('Thomas Mustermann')
+                    ->setAddressLine2('1000 Lausanne')
+                    ->setCountry('CH'),
+                "Thomas Mustermann\nCH-1000 Lausanne"
+            ],
+        ];
+    }
+}
