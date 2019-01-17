6619f1febdbbfccb3bb8f20ea042182ec61a8209
Manuel Reinhard <manu@sprain.ch>
2019-01-17 18:02:50 +0100

    Add proof of validation screenshot

--- tests/TestData/TestDataTest.php
+++ tests/TestData/TestDataTest.php
@@ -29,6 +29,8 @@ class TestDataTest extends TestCase
             [__DIR__ . '/qr-minimal-setup.png', '00d82b2797622e24b4736b1974094c75'],
             [__DIR__ . '/qr-payment-information-without-amount.png', '82abf9e7e2efb63d2b8d1d3809b82ddd'],
             [__DIR__ . '/qr-ultimate-debtor.png', '835efdaebe41b0b30e3f967b7aa6a0cb'],
+
+            [__DIR__ . '/proof-of-validation.png', '726bd835f52cb98d3874309222f7a727'],
         ];
     }
 }
