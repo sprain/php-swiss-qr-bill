af1755c94d710d070f4adb3dd81b847ceaa47ca4
Manuel Reinhard <manu@sprain.ch>
2019-01-31 13:43:58 +0100

    Add loadValidatorMetadata to SelfValidatable interface

--- src/Validator/Interfaces/SelfValidatable.php
+++ src/Validator/Interfaces/SelfValidatable.php
@@ -3,10 +3,13 @@
 namespace Sprain\SwissQrBill\Validator\Interfaces;
 
 use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Mapping\ClassMetadataInterface;
 
 interface SelfValidatable
 {
     public function getViolations() : ConstraintViolationListInterface;
 
     public function isValid() : bool;
+
+    public static function loadValidatorMetadata(ClassMetadataInterface $metadata) : void;
 }
