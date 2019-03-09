d0e29c1d85abb8d163a638ae821b7e074071958e
Manuel Reinhard <manu@sprain.ch>
2019-03-09 20:29:07 +0100

    Simplify HtmlOutputTest

--- tests/PaymentPart/Output/HtmlOutput/HtmlOutputTest.php
+++ tests/PaymentPart/Output/HtmlOutput/HtmlOutputTest.php
@@ -12,7 +12,7 @@ class HtmlOutputTest extends TestCase
 {
     use TestQrBillCreatorTrait;
 
-    private $regenerateReferenceHtmlOutputs = true;
+    private $regenerateReferenceHtmlOutputs = false;
 
     /**
      * @dataProvider validQrBillsProvider
@@ -21,106 +21,18 @@ class HtmlOutputTest extends TestCase
     {
         $file = __DIR__ . '/../../../TestData/HtmlOutput/' . $name . '.html';
 
-        $output = (new HtmlOutput($qrBill, 'de'))->getPaymentPart();
+        $output = (new HtmlOutput($qrBill, 'en'))->getPaymentPart();
 
         if ($this->regenerateReferenceHtmlOutputs) {
            file_put_contents($file, $output);
         }
 
+        $crawler = new Crawler($output);
+        $this->assertEquals(1, $crawler->filter('td#qr-bill-payment-part')->count());
+
         $this->assertSame(
             file_get_contents($file),
             $output
         );
-    }
-
-    /**
-     * @dataProvider qrBillProvider
-     */
-    public function testOutputIsHtml($qrBill)
-    {
-        $output = (new HtmlOutput($qrBill, 'de'))->getPaymentPart();
-
-        $crawler = new Crawler($output);
-        $this->assertEquals(1, $crawler->filter('td#qr-bill-payment-part')->count());
-    }
-
-    public function qrBillProvider()
-    {
-        return [
-            [
-                $this->createQrBill([
-                    'header',
-                    'creditorInformationQrIban',
-                    'creditor',
-                    'paymentAmountInformation',
-                    'paymentReferenceQr'
-                ])
-            ],
-            [
-                $this->createQrBill([
-                    'header',
-                    'creditorInformationQrIban',
-                    'creditor',
-                    'paymentAmountInformationWithoutAmount',
-                    'paymentReferenceQr'
-                ])
-            ],
-            [
-                $this->createQrBill([
-                    'header',
-                    'creditorInformationIban',
-                    'creditor',
-                    'paymentAmountInformation',
-                    'paymentReferenceScor'
-                ])
-            ],
-            [
-                $this->createQrBill([
-                    'header',
-                    'creditorInformationIban',
-                    'creditor',
-                    'paymentAmountInformation',
-                    'paymentReferenceScor'
-                ])
-            ],
-            [
-                $this->createQrBill([
-                    'header',
-                    'creditorInformationIban',
-                    'creditor',
-                    'paymentAmountInformation',
-                    'paymentReferenceNon'
-                ])
-            ],
-            [
-                $this->createQrBill([
-                    'header',
-                    'creditorInformationQrIban',
-                    'creditor',
-                    'paymentAmountInformation',
-                    'paymentReferenceQr',
-                    'ultimateDebtor'
-                ])
-            ],
-            [
-                $this->createQrBill([
-                    'header',
-                    'creditorInformationQrIban',
-                    'creditor',
-                    'paymentAmountInformation',
-                    'paymentReferenceQr',
-                ])
-            ],
-            [
-                $this->createQrBill([
-                    'header',
-                    'creditorInformationQrIban',
-                    'creditor',
-                    'paymentAmountInformation',
-                    'paymentReferenceQr',
-                    'additionalInformation'
-                ])
-            ]
-        ];
     }
 }
