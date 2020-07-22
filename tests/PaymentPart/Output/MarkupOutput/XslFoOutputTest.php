1753540364d38d9c9aebd8575d1f15766aea68a4
Lukas Maeglin <lm@cyon.ch>
2020-07-22 23:19:23 +0200

    Adds first working version of xsl-fo testdata for unit tests. The xml data is still in an alpha state.

--- tests/PaymentPart/Output/MarkupOutput/XslFoOutputTest.php
+++ tests/PaymentPart/Output/MarkupOutput/XslFoOutputTest.php
@@ -12,51 +12,53 @@ class XslFoOutputTest extends TestCase
 {
     use TestQrBillCreatorTrait;
 
-//    /**
-//     * @dataProvider validQrBillsProvider
-//     */
-//    public function testValidQrBills(string $name, QrBill $qrBill)
-//    {
-//        $variations = [
-//            [
-//                'printable' => false,
-//                'format' => QrCode::FILE_FORMAT_SVG,
-//                'file' => __DIR__ . '/../../../TestData/XslFoOutput/' . $name . '.svg.xml'
-//            ],
-//            [
-//                'printable' => true,
-//                'format' => QrCode::FILE_FORMAT_SVG,
-//                'file' => __DIR__ . '/../../../TestData/XslFoOutput/' . $name . '.svg.print.xml'
-//            ],
-//            [
-//                'printable' => false,
-//                'format' => QrCode::FILE_FORMAT_PNG,
-//                'file' => __DIR__ . '/../../../TestData/XslFoOutput/' . $name . '.png.xml'
-//            ],
-//            [
-//                'printable' => true,
-//                'format' => QrCode::FILE_FORMAT_PNG,
-//                'file' => __DIR__ . '/../../../TestData/XslFoOutput/' . $name . '.png.print.xml'
-//            ]
-//        ];
-//
-//        foreach ($variations as $variation) {
-//            $file = $variation['file'];
-//
-//            $xslFoOutput = (new XslFoOutput($qrBill, 'en'));
-//            $output = $xslFoOutput
-//                ->setPrintable($variation['printable'])
-//                ->setQrCodeImageFormat($variation['format'])
-//                ->getPaymentPart();
-//
-//            if ($this->regenerateReferenceFiles) {
-//               file_put_contents($file, $output);
-//            }
-//
-//            $this->assertSame(
-//                file_get_contents($file),
-//                $output
-//            );
-//        }
-//    }
+    /**
+     * @dataProvider validQrBillsProvider
+     */
+    public function testValidQrBills(string $name, QrBill $qrBill)
+    {
+        $variations = [
+            [
+                'printable' => false,
+                'format' => QrCode::FILE_FORMAT_SVG,
+                'file' => __DIR__ . '/../../../TestData/XslFoOutput/' . $name . '.svg.xml'
+            ],
+            [
+                'printable' => true,
+                'format' => QrCode::FILE_FORMAT_SVG,
+                'file' => __DIR__ . '/../../../TestData/XslFoOutput/' . $name . '.svg.print.xml'
+            ],
+            [
+                'printable' => false,
+                'format' => QrCode::FILE_FORMAT_PNG,
+                'file' => __DIR__ . '/../../../TestData/XslFoOutput/' . $name . '.png.xml'
+            ],
+            [
+                'printable' => true,
+                'format' => QrCode::FILE_FORMAT_PNG,
+                'file' => __DIR__ . '/../../../TestData/XslFoOutput/' . $name . '.png.print.xml'
+            ]
+        ];
+
+        foreach ($variations as $variation) {
+            $file = $variation['file'];
+
+            $xslFoOutput = (new XslFoOutput($qrBill, 'en'));
+            $output = $xslFoOutput
+                ->setPrintable($variation['printable'])
+                ->setQrCodeImageFormat($variation['format'])
+                ->getPaymentPart();
+
+            $this->assertNotEmpty($output);
+
+            if ($this->regenerateReferenceFiles) {
+               file_put_contents($file, $output);
+            }
+
+            $this->assertSame(
+                file_get_contents($file),
+                $output
+            );
+        }
+    }
 }
