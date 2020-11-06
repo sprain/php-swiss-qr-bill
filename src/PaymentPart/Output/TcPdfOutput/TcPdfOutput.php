fa93c4a0a1e2cd84a354883f101979b2c50083f8
Manuel Reinhard <manu@sprain.ch>
2020-11-06 12:39:26 +0100

    Fix duplicated offset of lines in TcPdfOutput

--- src/PaymentPart/Output/TcPdfOutput/TcPdfOutput.php
+++ src/PaymentPart/Output/TcPdfOutput/TcPdfOutput.php
@@ -231,8 +231,8 @@ final class TcPdfOutput extends AbstractOutput implements OutputInterface
     {
         if (!$this->isPrintable()) {
             $this->tcPdf->SetLineStyle(array('width' => 0.1, 'color' => array(0, 0, 0)));
-            $this->printLine(2 + $this->offsetX, 193 + $this->offsetY, 208 + $this->offsetX, 193 + $this->offsetY);
-            $this->printLine(62 + $this->offsetX, 193 + $this->offsetY, 62 + $this->offsetX, 296 + $this->offsetY);
+            $this->printLine(2, 193, 208, 193);
+            $this->printLine(62, 193, 62, 296);
             $this->tcPdf->SetFont(self::FONT, '', self::FONT_SIZE_FURTHER_INFORMATION);
             $this->SetY(188);
             $this->SetX(5);
