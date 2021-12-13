46048faf696e6cc0199dfd500cb50f2f6bd8b74d
Manuel Reinhard <manu@sprain.ch>
2021-12-13 13:58:35 +0100

    Update to dev-master of endroid/qr-code v4 until a new stable release is available

--- tests/TestData/QrCodes/TestDataTest.php
+++ tests/TestData/QrCodes/TestDataTest.php
@@ -23,19 +23,19 @@ class TestDataTest extends TestCase
     public function qrFileProvider(): array
     {
         return [
-            [__DIR__ . '/qr-additional-information.png', 'e5a630cb9e59a2219a4b4212f787f66b'],
+            [__DIR__ . '/qr-additional-information.png', 'c690b3c552cb31057a34d1bbe1e3a158'],
             [__DIR__ . '/qr-alternative-schemes.png', 'ca22587f45609486ec9128f8bfb9ef83'],
             [__DIR__ . '/qr-full-set.png', 'ae3aa21373bb4b6ad61a8df96995f06b'],
-            [__DIR__ . '/qr-international-ultimate-debtor.png', 'c8585f93e5f0f5f7c89c8c1ee2e89eda'],
-            [__DIR__ . '/qr-minimal-setup.png', 'e0186a62a5d4ffeaa66903e0587cd6e8'],
-            [__DIR__ . '/qr-payment-information-without-amount.png', 'aa02af5ebfa9d79067590d273fb1a063'],
-            [__DIR__ . '/qr-payment-information-without-amount-but-debtor.png', '0e664c61803b33ceb7e5f687c748b001'],
-            [__DIR__ . '/qr-payment-information-zero-amount.png', 'ef897e7a872b684e06304dcc83e83ddc'],
-            [__DIR__ . '/qr-payment-reference-non.png', 'b7a3423d03db5b8d581f14ad56efd35f'],
-            [__DIR__ . '/qr-payment-reference-scor.png', 'a79f86815a768d724132066d2c4490fb'],
-            [__DIR__ . '/qr-ultimate-debtor.png', '151cc97a1fd8059db0f3276e0e530269'],
+            [__DIR__ . '/qr-international-ultimate-debtor.png', '3178b54237dbbf43df99ea98bba82aaa'],
+            [__DIR__ . '/qr-minimal-setup.png', '246e856c5c75e92ad9e70298e870d957'],
+            [__DIR__ . '/qr-payment-information-without-amount.png', 'd21e7106158945a52c7b2be00fbd5369'],
+            [__DIR__ . '/qr-payment-information-without-amount-but-debtor.png', '67b382fdaa8cd69eb328862d8393fb9f'],
+            [__DIR__ . '/qr-payment-information-zero-amount.png', '66c1373bac50b98705d94b33462a72c6'],
+            [__DIR__ . '/qr-payment-reference-non.png', '5843f882b1883f8202c43c17fa07ae86'],
+            [__DIR__ . '/qr-payment-reference-scor.png', '4ef959e7b428650ec4198491a6d91f1c'],
+            [__DIR__ . '/qr-ultimate-debtor.png', '9d1d257c2b65d9d04d4d7a20ced6ef1a'],
 
-            [__DIR__ . '/proof-of-validation.png', '5a2ef1da7ce12c26b6980b73ce756cb7'],
+            [__DIR__ . '/proof-of-validation.png', '1eb684f1fb5e2ebab835ff60862977e5'],
         ];
     }
 }
