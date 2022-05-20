88b653e5b65dedd59f5429d53dd976c9829c66e3
Severin J <severin.jany@kirchenweb.ch>
2022-05-20 14:28:52 +0200

    Resolve conflicts with another class in this namespace and code style.

--- example/PhpWordOutput/phpword-example.php
+++ example/PhpWordOutput/phpword-example.php
@@ -1,36 +1,35 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 ini_set('display_errors', 'on');
 
+use PhpOffice\PhpWord\PhpWord;
 use PhpOffice\PhpWord\Shared\Converter;
-use Sprain\SwissQrBill as QrBill;
-use Sprain\SwissQrBill\QrCode\QrCode;
 
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__.'/../../vendor/autoload.php';
 
 // 1. Let's load the base example to define the qr bill contents
-require __DIR__ . '/../example.php';
+require __DIR__.'/../example.php';
 // 2. Create a TCPDF instance (or use an existing one from your project)
-$phpword = new \PhpOffice\PhpWord\PhpWord();
+$phpword = new PhpWord();
 $section = $phpword->addSection([
-		'paperSize' => 'A4',
-		'orientation' => 'portrait',
-		'marginTop' => Converter::cmToTwip(2.0),
-		'marginLeft' => Converter::cmToTwip(2.0),
-		'marginBottom' => Converter::cmToTwip(2.0),
-		'marginRight' => Converter::cmToTwip(2.0),
-		'headerHeight' => Converter::cmToTwip(2.0),
-		'footerHeight' => Converter::cmToTwip(2.0),
+        'paperSize' => 'A4',
+        'orientation' => 'portrait',
+        'marginTop' => Converter::cmToTwip(2.0),
+        'marginLeft' => Converter::cmToTwip(2.0),
+        'marginBottom' => Converter::cmToTwip(2.0),
+        'marginRight' => Converter::cmToTwip(2.0),
+        'headerHeight' => Converter::cmToTwip(2.0),
+        'footerHeight' => Converter::cmToTwip(2.0),
 ]);
 // 3. Create a full payment part for TcPDF
 /** @noinspection PhpUndefinedVariableInspection */
-$output = new QrBill\PaymentPart\Output\PhpWordOutput\PhpWordOutput($qrBill, 'en', $phpword);
+$output = new \Sprain\SwissQrBill\PaymentPart\Output\PhpWordOutput\PhpWordOutput($qrBill, 'en', $phpword);
 $output
-    ->setPrintable(false)
-		->setQrCodeImageFormat(QrCode::FILE_FORMAT_PNG)
-    ->getPaymentPart();
+        ->setPrintable(false)
+        ->setQrCodeImageFormat(\Sprain\SwissQrBill\QrCode\QrCode::FILE_FORMAT_PNG)
+        ->getPaymentPart();
 // 4. For demo purposes, let's save the generated example in a file
-$examplePath = __DIR__ . "/phpword_example.docx";
+$examplePath = __DIR__."/phpword_example.docx";
 $phpword->save($examplePath, download: true);
 
 print "Word example created here : ".$examplePath;
